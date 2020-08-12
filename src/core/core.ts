1de967af8fd419cd7e762c9ac7c8d20c72d24f97
Igor Karfidoff <karfidoff@gmail.com>
2020-08-11 21:17:39 -0400

    JSX introduction

--- src/core/core.ts
+++ src/core/core.ts
@@ -3,9 +3,12 @@ import "reflect-metadata";
 export function AttributeProperty(target: {} | any, name?: PropertyKey) {
     const descriptor = {
       get(this: any) {
+        console.log("get " + String(name));
         return this.getAttribute(name);
       },
       set(value: any) {
+        console.log('set ' + String(name));
+        console.log(this);
         let oldValue = this.getAttribute(name);
         if (value) {
           this.setAttribute(name, value);
@@ -23,3 +26,27 @@ export function AttributeProperty(target: {} | any, name?: PropertyKey) {
     Object.defineProperty(target, name, descriptor);
     Reflect.defineMetadata(name, descriptor, target);
 }
+
+
+export const JSX = {
+  createElement(name: string, props: { [id: string]: string }, ...content: string[]) {
+    props = props || {};
+    const propsstr = Object.keys(props)
+      .map(key => {
+        const value = props[key];
+        if (key === "className") {
+          return `class=${value}`;
+        }
+        console.log(key);
+        console.log(value);
+        return `${key}='${value}'`;
+      })
+      .join(" ");
+    if (!name) {
+      return  `${content.join("")}`;
+    }
+    return content.length ? `<${name} ${propsstr}>${content.join("")}</${name}>` : `<${name} ${propsstr}/>`;
+  },
+};
+
+export default JSX;
